4d6d3ed06c350fa764d76fca80b83201562193f1
Llewellyn Falco <isidore@setgame.com>
2013-04-03 23:11:46 -0700

    Refactored Testing

---
+++ src/org/approvaltests/koans/tests/KoanUtils.java
@@ -0,0 +1,51 @@
+package org.approvaltests.koans.tests;
+
+import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
+
+import org.approvaltests.koans.helpers.Koans;
+
+public class KoanUtils
+{
+  public static void assertKoanReady(Koans koan, String method)
+  {
+    assertState(koan, method, false);
+  }
+  public static void assertKoan(Koans koan, String method, Object answer)
+  {
+    assertState(koan, method, false);
+    setAnswers(koan, answer);
+    assertState(koan, method, true);
+  }
+  private static void setAnswers(Koans koan, Object answer)
+  {
+    if (answer instanceof String)
+    {
+      koan.___ = (String) answer;
+    }
+    if (answer instanceof Integer)
+    {
+      koan.____ = (Integer) answer;
+    }
+  }
+  private static void assertState(Koans koan, String method, boolean passDesired)
+  {
+    boolean failed = false;
+    try
+    {
+      koan.getClass().getMethod(method, null).invoke(koan, null);
+    }
+    catch (Throwable e)
+    {
+      failed = true;
+    }
+    if (passDesired)
+    {
+      Assert.assertFalse(String.format("The Method '%s' will not solve", method), failed);
+    }
+    else
+    {
+      assertTrue(String.format("The Method '%s' is alread passing", method), failed);
+    }
+  }
+}
--- src/org/approvaltests/koans/tests/KoansTests.java
+++ src/org/approvaltests/koans/tests/KoansTests.java
@@ -1,9 +1,5 @@
 package org.approvaltests.koans.tests;
 
-import static org.junit.Assert.assertTrue;
-import junit.framework.Assert;
-
-import org.approvaltests.koans.helpers.Koans;
 import org.approvaltests.koans.lesson01.GettingStarted;
 import org.junit.Test;
 
@@ -12,55 +8,14 @@ public class KoansTests
   @Test
   public void TestLesson1() throws Exception
   {
-    assertKoan(new GettingStarted(), "NormalJunitAsserts", "Small String");
-    assertKoan(new GettingStarted(), "ApprovalHidesTheExpectation", "This is in the approved file");
-    assertKoan(new GettingStarted(), "ApprovalFileName", "GettingStarted.ApprovalFileName");
-    assertKoan(new GettingStarted(), "ApprovalFileName", "GettingStarted.ApprovalFileName");
-    assertKoanReady(new GettingStarted(), "___");
-    assertKoan(new GettingStarted(), "FileNames", "GettingStarted");
-    assertKoanReady(new GettingStarted(), "ChangingTheGoldenMaster");
-    assertKoan(new GettingStarted(), "VerifyObjects", 150);
-    assertKoan(new GettingStarted(), "SometimeYouNeedABetterToString", "smith");
-  }
-  private void assertKoanReady(Koans koan, String method)
-  {
-    assertState(koan, method, false);
-  }
-  public void assertKoan(Koans koan, String method, Object answer)
-  {
-    assertState(koan, method, false);
-    setAnswers(koan, answer);
-    assertState(koan, method, true);
-  }
-  private void setAnswers(Koans koan, Object answer)
-  {
-    if (answer instanceof String)
-    {
-      koan.___ = (String) answer;
-    }
-    if (answer instanceof Integer)
-    {
-      koan.____ = (Integer) answer;
-    }
-  }
-  private void assertState(Koans koan, String method, boolean passDesired)
-  {
-    boolean failed = false;
-    try
-    {
-      koan.getClass().getMethod(method, null).invoke(koan, null);
-    }
-    catch (Throwable e)
-    {
-      failed = true;
-    }
-    if (passDesired)
-    {
-      Assert.assertFalse(String.format("The Method '%s' will not solve", method), failed);
-    }
-    else
-    {
-      assertTrue(String.format("The Method '%s' is alread passing", method), failed);
-    }
+    KoanUtils.assertKoan(new GettingStarted(), "NormalJunitAsserts", "Small String");
+    KoanUtils.assertKoan(new GettingStarted(), "ApprovalHidesTheExpectation", "This is in the approved file");
+    KoanUtils.assertKoan(new GettingStarted(), "ApprovalFileName", "GettingStarted.ApprovalFileName");
+    KoanUtils.assertKoan(new GettingStarted(), "ApprovalFileName", "GettingStarted.ApprovalFileName");
+    KoanUtils.assertKoanReady(new GettingStarted(), "___");
+    KoanUtils.assertKoan(new GettingStarted(), "FileNames", "GettingStarted");
+    KoanUtils.assertKoanReady(new GettingStarted(), "ChangingTheGoldenMaster");
+    KoanUtils.assertKoan(new GettingStarted(), "VerifyObjects", 150);
+    KoanUtils.assertKoan(new GettingStarted(), "SometimeYouNeedABetterToString", "smith");
   }
 }
